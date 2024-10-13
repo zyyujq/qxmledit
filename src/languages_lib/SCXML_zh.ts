9dcee36e54fcfbff4c12d4b8f1613f66da48d4fc
JQ.YU <112557539+zyyujq@users.noreply.github.com>
2024-10-13 21:44:35 +0800

    Add files via upload

---
+++ src/languages_lib/SCXML_zh.ts
@@ -0,0 +1,162 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>MainWindow</source>
+        <translation>主窗口</translation>
+    </message>
+    <message>
+        <source>Raises an event.</source>
+        <translation>引发一个事件。</translation>
+    </message>
+    <message>
+        <source>If clause.</source>
+        <translation>If 条件语句。</translation>
+    </message>
+    <message>
+        <source>Else if clause. </source>
+        <translation>Else if 条件语句。 </translation>
+    </message>
+    <message>
+        <source>Else clause.</source>
+        <translation>Else 条件语句。</translation>
+    </message>
+    <message>
+        <source>Loop.</source>
+        <translation>Loop 循环语句。</translation>
+    </message>
+    <message>
+        <source>Log instruction.</source>
+        <translation>日志说明。</translation>
+    </message>
+    <message>
+        <source>Assign instruction.</source>
+        <translation>分配指令。</translation>
+    </message>
+    <message>
+        <source>A script.</source>
+        <translation>一条脚本。</translation>
+    </message>
+    <message>
+        <source>Send a message action.</source>
+        <translatorcomment>发送消息操作。</translatorcomment>
+        <translation>发送消息操作。</translation>
+    </message>
+    <message>
+        <source>Cancel an action.</source>
+        <translation>取消操作。</translation>
+    </message>
+    <message>
+        <source>Root element.</source>
+        <translation>根元素。</translation>
+    </message>
+    <message>
+        <source>A compound or atomic state.</source>
+        <translation>化合物或原子状态。</translation>
+    </message>
+    <message>
+        <source>A parallel state.</source>
+        <translation>平行状态。</translation>
+    </message>
+    <message>
+        <source>Top-level final state.</source>
+        <translation>顶级最终状态。</translation>
+    </message>
+    <message>
+        <source>Part or all of the data model.</source>
+        <translation>部分或全部数据模型。</translation>
+    </message>
+    <message>
+        <source>Executable content to be run upon entering this state.</source>
+        <translation>进入此状态时要运行的可执行内容。</translation>
+    </message>
+    <message>
+        <source>Executable content to be run when exiting this state.</source>
+        <translation>退出此状态时要运行的可执行内容。</translation>
+    </message>
+    <message>
+        <source>Outgoing transition from this state.</source>
+        <translation>从这个状态向外过渡。</translation>
+    </message>
+    <message>
+        <source>Initial state in states that have substates.</source>
+        <translation>有子状态的初始状态。</translation>
+    </message>
+    <message>
+        <source>Sequential substate of the parent state.</source>
+        <translation>父状态的顺序子状态。</translation>
+    </message>
+    <message>
+        <source>Parallel substate.</source>
+        <translation>平行子状态。</translation>
+    </message>
+    <message>
+        <source>Final substate.</source>
+        <translatorcomment>最终子状态。</translatorcomment>
+        <translation>最终子状态。</translation>
+    </message>
+    <message>
+        <source>A pseudo-state to record the last descendant state(s).</source>
+        <translation>用于记录最后一个子项状态的伪状态。</translation>
+    </message>
+    <message>
+        <source>Invokes an external service.</source>
+        <translation>调用外部服务。</translation>
+    </message>
+    <message>
+        <source>Executable content to be run upon entering the parallel element.</source>
+        <translation>进入并行元素后要运行的可执行内容。</translation>
+    </message>
+    <message>
+        <source>Executable content to be run when exiting this element.</source>
+        <translation>退出此元素时要运行的可执行内容。</translation>
+    </message>
+    <message>
+        <source>Parallel substate region.</source>
+        <translation>平行子状态区域。</translation>
+    </message>
+    <message>
+        <source>Nested set of parallel regions.</source>
+        <translation>嵌套的平行区域集。</translation>
+    </message>
+    <message>
+        <source>Records the state configuration the last time the system transitioned from it.</source>
+        <translation>记录系统上次从状态配置转换时的状态配置。</translation>
+    </message>
+    <message>
+        <source>Defines part or all of the data model.</source>
+        <translation>定义部分或全部数据模型。</translation>
+    </message>
+    <message>
+        <source>Initial state for a complex state element.</source>
+        <translation>复杂状态元素的初始状态。</translation>
+    </message>
+    <message>
+        <source>Data to be included in the done.state.id or done.invoke.id event.</source>
+        <translation>要包含在 done.state.id 或 done.ivoke.id 事件中的数据。</translation>
+    </message>
+    <message>
+        <source>A transition whose &apos;target&apos; is the default history configuration.</source>
+        <translation>其“目标”是默认历史配置的转换。</translation>
+    </message>
+    <message>
+        <source>Data element.</source>
+        <translatorcomment>数据元素。</translatorcomment>
+        <translation>数据元素。</translation>
+    </message>
+    <message>
+        <source>Data to include.</source>
+        <translation>要包括的数据。</translation>
+    </message>
+    <message>
+        <source>Data to send.</source>
+        <translation>要发送的数据。</translation>
+    </message>
+    <message>
+        <source>Executable content to message the data returned from the invoked component.</source>
+        <translation>可执行内容，用于发送从被调用组件返回的数据。</translation>
+    </message>
+</context>
+</TS>
