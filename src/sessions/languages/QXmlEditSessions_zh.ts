dd1f444de2504c20ec2f91fb4540435833cd0aad
JQ.YU <112557539+zyyujq@users.noreply.github.com>
2024-10-13 21:24:46 +0800

    Add files via upload

---
+++ src/sessions/languages/QXmlEditSessions_zh.ts
@@ -0,0 +1,367 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>SessionsManagementDialog</name>
+    <message>
+        <source>Id</source>
+        <translation>ID</translation>
+    </message>
+    <message>
+        <source>Name</source>
+        <translation>名称</translation>
+    </message>
+    <message>
+        <source>Count</source>
+        <translation>计数</translation>
+    </message>
+    <message>
+        <source>Error reading session data</source>
+        <translation>读取会话数据时出错</translation>
+    </message>
+    <message>
+        <source>Last Access</source>
+        <translation>最近访问</translation>
+    </message>
+    <message>
+        <source>Select a session.</source>
+        <translation>选择一个会话。</translation>
+    </message>
+    <message>
+        <source>Data copied in the clipboard.</source>
+        <translation>剪贴板中复制的数据。</translation>
+    </message>
+    <message>
+        <source>Description</source>
+        <translation>描述</translation>
+    </message>
+    <message>
+        <source>error retrieving session data</source>
+        <translation>检索会话数据时出错</translation>
+    </message>
+    <message>
+        <source>Created</source>
+        <translation>创建</translation>
+    </message>
+    <message>
+        <source>Delete the selected session?</source>
+        <translation>删除所选会话？</translation>
+    </message>
+    <message>
+        <source>An error occurred deleting the session data</source>
+        <translation>删除会话数据时出错</translation>
+    </message>
+</context>
+<context>
+    <name>SessionDetailDialog</name>
+    <message>
+        <source>---</source>
+        <translation>---</translation>
+    </message>
+    <message>
+        <source>Name:</source>
+        <translation>名称:</translation>
+    </message>
+    <message>
+        <source>Edit file</source>
+        <translation>编辑文件</translation>
+    </message>
+    <message>
+        <source>Last access:</source>
+        <translation>最近访问:</translation>
+    </message>
+    <message>
+        <source>Description:</source>
+        <translation>描述:</translation>
+    </message>
+    <message>
+        <source>Export file data</source>
+        <translation>导出文件数据</translation>
+    </message>
+    <message>
+        <source>Error reading session data.</source>
+        <translation>读取会话数据时出错。</translation>
+    </message>
+    <message>
+        <source>Session Detail</source>
+        <translation>会话详细信息</translation>
+    </message>
+    <message>
+        <source>Data copied in the clipboard.</source>
+        <translation>剪贴板中复制的数据。</translation>
+    </message>
+    <message>
+        <source>Error saving session data</source>
+        <translation>保存会话数据时出错</translation>
+    </message>
+</context>
+<context>
+    <name>SessionsManagement</name>
+    <message>
+        <source>---</source>
+        <translation>---</translation>
+    </message>
+    <message>
+        <source>----</source>
+        <translation>----</translation>
+    </message>
+    <message>
+        <source>Name:</source>
+        <translation>名称:</translation>
+    </message>
+    <message>
+        <source>Edit file</source>
+        <translation>编辑文件</translation>
+    </message>
+    <message>
+        <source>Session list</source>
+        <translation>会话列表</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>删除</translation>
+    </message>
+    <message>
+        <source>Description:</source>
+        <translation>描述:</translation>
+    </message>
+    <message>
+        <source>Export file data</source>
+        <translation>导出文件数据</translation>
+    </message>
+    <message>
+        <source>New...</source>
+        <translation>新建...</translation>
+    </message>
+    <message>
+        <source>Session management</source>
+        <translation>会话管理</translation>
+    </message>
+    <message>
+        <source>Activate</source>
+        <translation>激活</translation>
+    </message>
+    <message>
+        <source>Edit...</source>
+        <translation>编辑...</translation>
+    </message>
+    <message>
+        <source>Close</source>
+        <translation>关闭</translation>
+    </message>
+</context>
+<context>
+    <name>SessionAccessDataModel</name>
+    <message>
+        <source>Date</source>
+        <translation>日期</translation>
+    </message>
+    <message>
+        <source>Path</source>
+        <translation>路径</translation>
+    </message>
+</context>
+<context>
+    <name>SessionDetailWidget</name>
+    <message>
+        <source>Form</source>
+        <translation>窗口</translation>
+    </message>
+    <message>
+        <source>Files</source>
+        <translation>文件</translation>
+    </message>
+    <message>
+        <source>Accesses</source>
+        <translation>访问</translation>
+    </message>
+</context>
+<context>
+    <name>SessionDrawerWidget</name>
+    <message>
+        <source>Form</source>
+        <translation>窗口</translation>
+    </message>
+    <message>
+        <source>More...</source>
+        <translation>更多...</translation>
+    </message>
+    <message>
+        <source>search...</source>
+        <translation>搜索...</translation>
+    </message>
+    <message>
+        <source>Open or close the search panel.</source>
+        <translation>打开或关闭搜索面板。</translation>
+    </message>
+    <message>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;No session active.&lt;/span&gt;&lt;/p&gt;&lt;p&gt;You can create a new session.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;没有活动会话。&lt;/span&gt;&lt;/p&gt;&lt;p&gt;您可以创建新会话。&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <source>Create a new session...</source>
+        <translation>创建新会话...</translation>
+    </message>
+</context>
+<context>
+    <name>SessionStateWidget</name>
+    <message>
+        <source>Form</source>
+        <translation>窗口</translation>
+    </message>
+    <message>
+        <source>Session state</source>
+        <translation>会话状态</translation>
+    </message>
+</context>
+<context>
+    <name>SessionFilesDataModel</name>
+    <message>
+        <source>Path</source>
+        <translation>路径</translation>
+    </message>
+    <message>
+        <source>Count</source>
+        <translation>计数</translation>
+    </message>
+    <message>
+        <source>Description</source>
+        <translation>描述</translation>
+    </message>
+</context>
+<context>
+    <name>SessionDataModel</name>
+    <message>
+        <source>Session data</source>
+        <translation>会话数据</translation>
+    </message>
+</context>
+<context>
+    <name>SessionFileAccess</name>
+    <message>
+        <source>%1
+  last accessed on %2
+  last updated on %3
+  size %4
+  accessed %5 times</source>
+        <translation>%1
+  上次访问时间为 %2
+  上次更新于 %3
+  大小 %4
+  访问 %5 次</translation>
+    </message>
+    <message>
+        <source>%1 (%2)</source>
+        <translation>%1 (%2)</translation>
+    </message>
+</context>
+<context>
+    <name>SessionStateWidgetPrivate</name>
+    <message>
+        <source>State unknown</source>
+        <translation>状态未知</translation>
+    </message>
+    <message>
+        <source>Session &apos;%1&apos; active.
+%2</source>
+        <translation>会话 &apos;%1&apos; 处于活动状态。
+%2</translation>
+    </message>
+    <message>
+        <source>Default data management active.</source>
+        <translation>默认数据管理处于活动状态。</translation>
+    </message>
+    <message>
+        <source>Default data management paused.</source>
+        <translation>默认数据管理已暂停。</translation>
+    </message>
+    <message>
+        <source>Session &apos;%1&apos; paused.
+%2</source>
+        <translation>会话 &apos;%1&apos; 已暂停。
+%2</translation>
+    </message>
+    <message>
+        <source>No session.</source>
+        <translation>没有会话。</translation>
+    </message>
+</context>
+<context>
+    <name>SessionManager::Private</name>
+    <message>
+        <source>unable to read sessions data.</source>
+        <translation>无法读取会话数据。</translation>
+    </message>
+</context>
+<context>
+    <name>SessionSummary</name>
+    <message>
+        <source>
+Most recent folders</source>
+        <translation>
+最新文件夹</translation>
+    </message>
+    <message>
+        <source>
+Most used</source>
+        <translation>
+使用</translation>
+    </message>
+    <message>
+        <source>
+Recent</source>
+        <translation>
+最近</translation>
+    </message>
+</context>
+<context>
+    <name>SessionCategoryAccess</name>
+    <message>
+        <source>CATEGORY</source>
+        <translation>类别</translation>
+    </message>
+</context>
+<context>
+    <name>SQLLiteDataAccess::Private</name>
+    <message>
+        <source>No detail</source>
+        <translation>无详细信息</translation>
+    </message>
+    <message>
+        <source>No profile</source>
+        <translation>无配置文件</translation>
+    </message>
+    <message>
+        <source>can&apos;t open sessions database:%1</source>
+        <translation>无法打开会话数据库: %1</translation>
+    </message>
+    <message>
+        <source>Out of memory.</source>
+        <translation>内存溢出。</translation>
+    </message>
+</context>
+<context>
+    <name>SessionDirectoryAccess</name>
+    <message>
+        <source>%1
+  last access: %2</source>
+        <translation>%1
+  最近访问: %2</translation>
+    </message>
+</context>
+<context>
+    <name>SessionRequester</name>
+    <message>
+        <source>New session name:</source>
+        <translation>新会话名称:</translation>
+    </message>
+    <message>
+        <source>OK</source>
+        <translation>确定</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>取消</translation>
+    </message>
+</context>
+</TS>
